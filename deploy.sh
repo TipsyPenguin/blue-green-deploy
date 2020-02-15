d028ee9603c618a4d9a122988caaa81e9da36275
Omelchenko Aleksey <ladenmc@gmail.com>
2020-02-15 18:27:02 +0300

    Add deploy info

--- deploy.sh
+++ deploy.sh
@@ -19,3 +19,9 @@ docker tag $IMAGE_NAME:$1 $IMAGE_NAME:${new_state}
 
 echo "Update the ${new_state} container"
 docker-compose up -d ${new_state}
+
+echo "Update deploy info for ${new_state} container"
+echo "{
+    \"version\": \"$1\",
+    \"state\": \"$new_state\"
+}" > ./docker/nginx/www/${new_state}.json
